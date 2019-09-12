6e82037c8c5c3dab5eb5c7aeea79b4c740aab068
Wesley Gomes <31673329+GomesWesley@users.noreply.github.com>
2019-09-12 19:47:12 -0300

    Update yocto-qt5.sh

--- yocto-qt5.sh
+++ yocto-qt5.sh
@@ -1,8 +1,9 @@
 apt-get clean && apt-get update && apt-get install -y locales
+locale-gen en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
-apt-get install gawk wget git-core diffstat unzip texinfo gcc-multilib \
+apt-get -y install gawk wget git-core diffstat unzip texinfo gcc-multilib \
      build-essential chrpath socat cpio python python3 python3-pip python3-pexpect \
      xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev \
      xterm
